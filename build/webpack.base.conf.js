478f2c426464e47e2eaa4b2f4b715daa6c77252c
corpglory-dev <dev@corpglory.com>
2020-10-22 20:32:57 +0300

    build && demo

---
+++ build/webpack.base.conf.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+
+function resolve(dir) {
+  return path.join(__dirname, '..', dir)
+}
+
+module.exports = {
+  context: resolve('src'),
+  entry: './index.ts',
+  plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: resolve('dist'),
+    libraryTarget: 'umd',
+    umdNamedDefine: true
+  }
+};
